810c1938d8ab2bdac382f802e6df3b9d5941e3f8
Crystal Knight <crystalknightcodes@gmail.com>
2020-09-23 11:55:18 -0400

    Implemented command line arguments

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -7,5 +7,60 @@ from cpu import *
 
 cpu = CPU()
 
-cpu.load()
+# Command Line Arguments (CLA)
+cla = {}
+
+if len(sys.argv) > 1:
+    for index in range(1, len(sys.argv)):
+        cla = sys.argv[index]
+
+        #Split arguments into resources
+        resource = cla.split('=', 1)
+        #Ignore leading dashes
+        if resource[0].startswith('-') or resource[0].startswith('--'):
+            pass
+
+        if len(resource) == 2:
+            #Resource has value
+            cla[resource[0]] = resource[1]
+        elif len(resource) == 1:
+            cla[resource[0]] = 1
+        else:
+            #Skip if invalid
+            pass
+
+print("Command Line Arguments:", cla)
+
+file_name = cla.get('file', ls8/examples/interrupts.ls8)
+base = cla.get('base', 2)
+
+if file_name:
+    try:
+        memory = [0] * 256
+        address = 0
+
+        with open(file_name) as open_file:
+            for line in open_file:
+                line = line.strip()
+                line = line.split('#', 1)[0]
+
+                if address < len(memory):
+                    try:
+                        line = int(line, base)
+                        memory[address] = line
+                        address += 1
+                    except ValueError:
+                        pass
+                else:
+                    print("Memory overflow.  Load not completed.")
+                    break
+
+        cpu.load(memory)
+    except FileNotFoundError:
+        print(f"Unable to find {file_name}.")
+        sys.exit(1)
+
+else:
+    cpu.load(second="Hello, World!")
+    
 cpu.run()
